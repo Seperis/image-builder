e7fd63df9509bf9fa893b751e515a7b508d667ed
seperis <seperis@gmail.com>
2024-01-29 12:24:13 -0600

    Updated

--- local/test_this.sh
+++ local/test_this.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 quote_para="s/\"//g"
 arr=( release image_type deb_distribution deb_codename deb_arch )
-ps_scp="arm64_min_bkwm_12.4_v6.1ti_play.conf"
-o_name=$( cat "$ps_scp" | grep -w "image_type" | cut -d "=" -f2 )
-p_name=$( sed -e 's/\"//g' <<< "$o_name" )
-q_name=$( cat "$ps_scp" | grep -w "image_type" | cut -d "=" -f2 | sed -e 's/\"//g' )
-echo $o_name
-echo $p_name
-echo $q_name
+ps_scp="local/arm64_min_bkwm_12.4_v6.1ti_play.conf"
+x_name=$( cat "$ps_scp" | grep -w "deb_codename" | cut -d "=" -f1 )
+echo $x_name
+y_name=$( cat "$ps_scp" | grep -w "deb_codename" | sed -n 2p | cut -d "=" -f2 )
+echo $y_name
+z_name=$( cat "$ps_scp" | grep -w "deb_codename" | sed -n 2p | cut -d "=" -f2 | sed -e 's/\"//g' )
+echo $z_name
+#o_name=$( cat "$ps_scp" | grep -w "image_type" | cut -d "=" -f2 )
+#p_name=$( sed -e 's/\"//g' <<< "$o_name" )
+#q_name=$( cat "$ps_scp" | grep -w "image_type" | cut -d "=" -f2 | sed -e 's/\"//g' )
+#echo $o_name
+#echo $p_name
+#echo $q_name
